1ef05c88bbd99857438bcaf3ae33fe3664410903
Carlos Uribe <corvato22@gmail.com>
2021-10-25 22:48:37 -0500

    Funcionalidad Login

--- scripts/usuarios.js
+++ scripts/usuarios.js
@@ -2,21 +2,18 @@ const login = document.querySelector('#formulario');
 const API_URL = 'http://localhost:4002/usuarios';
 
 login.addEventListener('click', async e => {
-    // const name = document.querySelector('#name').value;
-    // const lastName = document.querySelector('#lastName').value;
-    // const email = document.querySelector('#email').value;
-    // const url = await fetch(API_URL, {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         name,
-    //         lastName,
-    //         email
-    //     }),
-    //     headers: {
-    //         "Content-Type": "application/json; charset=UTF-8"
-    //     }
-    // });
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    
+    await fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            email
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    });
     alert('Funciona')
 })
-
-/*  */
