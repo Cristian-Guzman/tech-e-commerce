4fc7685809f163674aa2a6110c7dbe1746a9b1bc
Cristian Guzman <cristiangt1813@gmail.com>
2021-10-25 22:24:42 -0500

    Agregando el json de usuarios

--- scripts/usuarios.js
+++ scripts/usuarios.js
@@ -1,20 +1,22 @@
-const formulario = document.querySelector('#formulario');
-const API_URL = 'http://localhost:4000/usuarios';
+const login = document.querySelector('#formulario');
+const API_URL = 'http://localhost:4002/usuarios';
 
-formulario.addEventListener('submit', async e => {
-    const name = document.querySelector('#name').value;
-    const lastName = document.querySelector('#lastName').value;
-    const email = document.querySelector('#email').value;
-    const url = await fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            name,
-            lastName,
-            email
-        }),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        }
-    });
-    e.preventDefault();
+login.addEventListener('click', async e => {
+    // const name = document.querySelector('#name').value;
+    // const lastName = document.querySelector('#lastName').value;
+    // const email = document.querySelector('#email').value;
+    // const url = await fetch(API_URL, {
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         name,
+    //         lastName,
+    //         email
+    //     }),
+    //     headers: {
+    //         "Content-Type": "application/json; charset=UTF-8"
+    //     }
+    // });
+    alert('Funciona')
 })
+
+/*  */
